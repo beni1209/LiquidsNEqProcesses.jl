1d4504c2556317d92efe0859324da60678e162ba
Pablo Zubieta <pabloferz@yahoo.com.mx>
2019-08-22 12:33:47 -0500

    Begin implementation

--- src/LiquidsNEqProcesses.jl
+++ src/LiquidsNEqProcesses.jl
@@ -1,5 +1,20 @@
 module LiquidsNEqProcesses
 
-greet() = print("Hello World!")
+
+### Imports
+using Reexport
+using Parameters
+using LinearAlgebra: I
+
+@reexport using LiquidsDynamics
+
+
+### Exports
+#export asymptotics, dynamics, dynamics!, initialize_dynamics
+
+
+### Implementation
+#include("utils.jl")
+
 
 end # module
