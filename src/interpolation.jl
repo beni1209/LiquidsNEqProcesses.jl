5c010d56e333f50925e0435f7ccdcf9bf4be258d
Pablo Zubieta <pabloferz@yahoo.com.mx>
2019-08-27 14:49:09 -0500

    Changes for handling different types

--- src/interpolation.jl
+++ src/interpolation.jl
@@ -1,8 +1,8 @@
 # Struct for allocating the vectors necessary for interpolation.
-struct Interpolator{T}
-    A ::Vector{T} # coefficient for "u"
-    Sf::Vector{T} # pseudo-structure of the final state
-	ΔS::Vector{T} # difference between initial structure and Sf
+struct Interpolator{U, V}
+    A ::Vector{U} # coefficient for `u`
+    Sf::Vector{V} # pseudo-structure of the final state
+    ΔS::Vector{V} # difference between initial structure and `Sf`
 end
 
 function Interpolator(S₀::T, S₁::T) where {T}
@@ -12,13 +12,16 @@ function Interpolator(S₀::T, S₁::T) where {T}
     K  = nodes(S₀.grid)
     Sf = project.(S₁.f.(K))
     ΔS = project.(S₀.f.(K)) .- Sf
-    A  = project.(LiquidsDynamics.bsfactors(-2D₀, K, Sf)) .* inv.(Sf)
+    A  = LiquidsDynamics.bsfactors(-2D₀, K, Sf) .* inv.(Sf)
 
-    return Interpolator{eltype(A)}(A, Sf, ΔS)
+    U = eltype(A)
+    V = eltype(Sf)
+
+    return Interpolator{U, V}(A, Sf, ΔS)
 end
 
 function interpolate!(S, interpolator, u)
     @unpack A, Sf, ΔS = interpolator
-    @. S =muladd(ΔS, exp(A * u),Sf)
+    @. S = muladd(ΔS, exp(reduce_dof(A * u)), Sf)
     return S
 end
